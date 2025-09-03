98037dbed0a6a1b0aee73a8e54c85d8b7a4e8d78
David_Armah <davarmah@gmail.com>
2025-09-03 11:21:20 +0000

    created a user profile using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,10 +2,12 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import WelcomeMessage from './components/WelcomeMessage'
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 
 
 function App() {
@@ -37,6 +39,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="David" age={24} bio="I love music, i am an organist" />
     </>
   )
 }
